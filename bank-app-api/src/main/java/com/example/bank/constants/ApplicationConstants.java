8ed239f02b5506c042f52c93d26550200e3bc962
David Cortaberria <david.cortabarria@gmail.com>
2025-05-27 15:42:30 +0200

    empty option1

--- bank-app-api/src/main/java/com/example/bank/constants/ApplicationConstants.java
+++
@@ -1,9 +0,0 @@
-package com.example.bank.constants;
-
-public final class ApplicationConstants {
-
-    public static final String JWT_SUBJECT = "JWT-Token";
-    public static final String JWT_HEADER = "Authorization";
-    public static final String JWT_HEADER_REFRESH = "Authorization-Refresh";
-
-}
